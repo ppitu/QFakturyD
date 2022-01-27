d4561abe184668167a5b1fcdaccc976ba17a57ca
Piotrek <piotr.chmura.1998@o2.pl>
2022-01-27 22:53:54 +0100

    Init test subproject

---
+++ qfaktury-test/main.cpp
@@ -0,0 +1,17 @@
+#include <QTest>
+
+#include "tst_firsttest.h"
+
+int main(int argc, char** argv)
+{
+    int status = 0;
+
+    auto ASSERT_TEST = [&status, argc, argv](QObject* obj) {
+        status |= QTest::qExec(obj, argc, argv);
+        delete obj;
+    };
+
+    ASSERT_TEST(new FirstTest());
+
+    return status;
+}
---
+++ qfaktury-test/tst_firsttest.cpp
@@ -0,0 +1,20 @@
+#include <QtTest>
+
+#include "tst_firsttest.h"
+
+// add necessary includes here
+
+FirstTest::FirstTest()
+{
+
+}
+
+FirstTest::~FirstTest()
+{
+
+}
+
+void FirstTest::test_case1()
+{
+    QCOMPARE(14, 43);
+}
